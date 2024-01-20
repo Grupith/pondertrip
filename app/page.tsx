65ca979cae511061a94c3568cac215243df693af
Dylan Koss <dylanhkoss@gmail.com>
2024-01-20 00:27:47 -0600

    updated UI

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   const { darkMode, toggleDarkMode } = useTheme()
   return (
     <main className={`${darkMode ? "dark" : ""} `}>
-      <div className="pt-2 min-h-screen overflow-hidden fade-in-0 animate-in animate-out duration-700 bg-white dark:bg-gray-900 dark:text-white">
+      <div className="pt-2 min-h-screen overflow-hidden fade-in-0 animate-in animate-out duration-700 bg-stone-300 dark:bg-slate-900 dark:text-white">
         <nav className="px-2 mt-2 flex justify-between sm:justify-around items-center">
-          <div className="flex items-center space-x-1 ">
+          <div className="flex items-center space-x-1 cursor-pointer ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -22,10 +22,9 @@ export default function Home() {
                 d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
               />
             </svg>
-
-            <span className="text-3xl font-medium">pondertrip</span>
+            <span className="text-2xl font-medium">pondertrip</span>
           </div>
-          <div className="flex items-center space-x-3">
+          <div className="flex items-center space-x-4">
             {/* Dark Mode Switcher */}
             <div className={`cursor-pointer`} onClick={toggleDarkMode}>
               <svg
@@ -43,7 +42,7 @@ export default function Home() {
                 />
               </svg>
             </div>
-            <button className="text-lg font-semibold border rounded-md shadow-md border-black/70 px-4 py-1 hover:scale-105 transition-all dark:border-white">
+            <button className="text-md font-medium rounded-md shadow-md bg-blue-700 px-4 py-1 hover:scale-105 transition-all text-white">
               Try now!
             </button>
           </div>
@@ -53,12 +52,12 @@ export default function Home() {
           What we do, is in our name
         </h3>
         <div className="flex justify-evenly mt-16 flex-wrap space-y-8 sm:space-y-0 dark:text-gray-300">
-          <div className="border border-black/50 w-80 p-3 rounded-lg shadow-lg dark:border-white">
+          <div className="w-80 p-6 rounded-lg shadow-lg bg-stone-400 dark:bg-slate-800">
             <h2 className="text-3xl font-extrabold mb-2">Ponder</h2>
             <p className="text-lg italic">noun</p>
             <p className="text-lg">To think deeply about something.</p>
           </div>
-          <div className="border border-black/50 w-80 p-3 rounded-lg shadow-lg dark:border-white">
+          <div className="w-80 p-6 rounded-lg shadow-lg bg-stone-400 dark:bg-slate-800">
             <h2 className="text-3xl font-extrabold mb-2">Trip</h2>
             <p className="text-lg italic">noun</p>
             <p className="text-lg">
@@ -70,8 +69,9 @@ export default function Home() {
         <h3 className="text-center text-4xl mt-20 font-medium">
           Organize all your trips with ease
         </h3>
+        <p className="text-center mt-20">"Screenshots of app"</p>
       </div>
-      <footer className="font-semibold text-sm text-center py-4 dark:bg-gray-900 dark:text-gray-300">
+      <footer className="font-semibold text-sm text-center py-2 bg-stone-300 shadow-md dark:bg-slate-900 dark:text-gray-300">
         <span>© 2024 Pondertrip. All rights reserved.</span>
       </footer>
     </main>
